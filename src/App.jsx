f00f23e4ce7bd5222d6455cabe3520323b22b7e7
Evgen <kapranov.evgeniy@gmail.com>
2024-10-25 22:16:59 +0300

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,110 @@ import AppHeader from "./components/app-header/App-Header";
 import Bur from "./components/burger-ingredients/Burger-Ingredients";
 import Constructor from "./components/burger-constructor/Burger-Constructor";
 import styles from "./app.module.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+
+const URL = "https://norma.nomoreparties.space/api/ingredients";
+const getData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
 
 function App() {
+  const [showModal, setShowModal] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const [ingredients, setIngredients] = React.useState([]);
+  React.useEffect(() => {
+    getData(URL)
+      .then((data) => setIngredients(data.data))
+      .catch(() => {
+        setError("При получении данных произошла ошибка!");
+      });
+  }, []);
+
+  console.log(ingredients);
+
+  // if (error) {
+  //   return <p style={{}}>{error}</p>;
+  // }
   return (
     <>
+      {/* {error && (
+        <div
+          style={{
+            position: "absolute",
+            display: "flex",
+            justifyContent: "center",
+
+            alignItems: "center",
+            backgroundColor: "red",
+            opacity: "0.9",
+            inset: 0,
+            zIndex: 100,
+            width: 400,
+            height: 400,
+          }}
+          onClick={() => setError(null)}
+        >
+          {error}
+          jhgjhgkhg
+        </div>
+      )} */}
+
       <AppHeader />
       <main className={styles.app_main}>
-        <Bur />
+        <Bur data={ingredients} />
         <Constructor />
+        {showModal && <Modal open={showModal} onClose={setShowModal} />}
+        <Button
+          htmlType="button"
+          type="primary"
+          onClick={() => setShowModal(!showModal)}
+        >
+          Открыть модальное окно
+        </Button>
       </main>
     </>
   );
 }
 
 export default App;
+
+const Modal = ({ open, onClose }) => {
+  console.log(open);
+  if (!open) {
+    return null;
+  }
+  return ReactDOM.createPortal(
+    <>
+      {
+        <div
+          style={{
+            position: "fixed",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "red",
+            opacity: "0.9",
+            inset: 0,
+            zIndex: 100,
+            width: "50vw",
+            height: " 50vh",
+          }}
+        >
+          modal
+          <Button
+            htmlType="button"
+            type="primary"
+            onClick={() => onClose(false)}
+          >
+            Закрыть модальное окно
+          </Button>
+        </div>
+      }
+    </>,
+    document.getElementById("modal")
+  );
+};
--- src/components/burger-ingredients/Burger-Ingredients.jsx
+++ src/components/burger-ingredients/Burger-Ingredients.jsx
@@ -4,107 +4,18 @@ import {
   Counter,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import React, { useState, useRef } from "react";
-import imgBun1 from "../../images/bun-01.png";
-import imgBun2 from "../../images/bun-02.png";
-import imgSauce1 from "../../images/sauce-01.png";
-import imgSauce2 from "../../images/sauce-02.png";
-import imgSauce3 from "../../images/sauce-03.png";
-import imgSauce4 from "../../images/sauce-04.png";
-import imgMeat1 from "../../images/meat-01.png";
-import imgMeat2 from "../../images/meat-02.png";
-import imgMeat3 from "../../images/meat-03.png";
-import imgMeat4 from "../../images/meat-04.png";
-import imgcheese1 from "../../images/cheese.png";
-import imgCore from "../../images/core.png";
-import imgMinerals from "../../images/mineral rings.png";
-import imgSalad from "../../images/salad.png";
-import imgSp from "../../images/sp 1.png";
 import styles from "./burger-ingredients.module.css";
 
-const buns = [
-  { id: "1", image: imgBun2, name: "Краторная булка N-200i", price: "20" },
-  {
-    id: "2",
-    image: imgBun1,
-    name: "Флюоресцентная булка R2-D3 2",
-    price: "20",
-  },
-];
-
-const sauces = [
-  { id: "1", image: imgSauce1, name: "Соус Spicy-X", price: "30" },
-  {
-    id: "2",
-    image: imgSauce2,
-    name: "Соус фирменный Space Sauce 2",
-    price: "30",
-  },
-  {
-    id: "3",
-    image: imgSauce3,
-    name: "Соус традиционный галактический",
-    price: "30",
-  },
-  {
-    id: "4",
-    image: imgSauce4,
-    name: "Соус с шипами Антарианского плоскоходца",
-    price: "30",
-  },
-];
-
-const fillings = [
-  {
-    id: "1",
-    image: imgMeat1,
-    name: "Филе Люминесцентного тетраодонтимформа",
-    price: "300",
-  },
-  {
-    id: "2",
-    image: imgMeat2,
-    name: "Мясо бессмертных моллюсков Protostomia",
-    price: "300",
-  },
-  {
-    id: "3",
-    image: imgMeat3,
-    name: "Говяжий метеорит (отбивная)",
-    price: "300",
-  },
-  {
-    id: "4",
-    image: imgMeat4,
-    name: "Биокотлета из марсианской Магнолии",
-    price: "300",
-  },
-  {
-    id: "5",
-    image: imgcheese1,
-    name: "Сыр с астероидной плесенью",
-    price: "80",
-  },
-  {
-    id: "6",
-    image: imgCore,
-    name: "Кристаллы марсианских альфа-сахаридов",
-    price: "80",
-  },
-  {
-    id: "7",
-    image: imgMinerals,
-    name: "Хрустящие минеральные кольца",
-    price: "80",
-  },
-  { id: "8", image: imgSalad, name: "Мини-салат Экзо-Плантаго", price: "80" },
-  { id: "9", image: imgSp, name: "Плоды фалленианского дерева", price: "80" },
-];
-
-function BurgerInredients() {
+function BurgerIngredients({ data }) {
   const [current, setCurrent] = useState("Булки");
+
   const bunRef = useRef(null);
   const sauceRef = useRef(null);
   const fillingRef = useRef(null);
+
+  const buns = data.filter((item) => item.type === "bun");
+  const sauces = data.filter((item) => item.type === "sauce");
+  const fillings = data.filter((item) => item.type === "main");
 
   const scrollToSection = (ref) => {
     ref.current.scrollIntoView({ behavior: "smooth" });
@@ -147,25 +58,26 @@ function BurgerInredients() {
       </div>
       <div className={`mt-10 ${styles.ingredients}`}>
         <WrapperGroup title={"Булки"} ref={bunRef}>
-          {buns.map(({ image, name, price, id }) => (
-            <Item key={id} image={image} name={name} price={price} />
+          {buns.map(({ image, name, price, _id }) => (
+            <Item key={_id} image={image} name={name} price={price} />
           ))}
         </WrapperGroup>
         <WrapperGroup title={"Соусы"} ref={sauceRef}>
-          {sauces.map(({ image, name, price, id }) => (
-            <Item key={id} image={image} name={name} price={price} />
+          {sauces.map(({ image, name, price, _id }) => (
+            <Item key={_id} image={image} name={name} price={price} />
           ))}
         </WrapperGroup>
         <WrapperGroup title={"Начинки"} ref={fillingRef}>
-          {fillings.map(({ image, name, price, id }) => (
-            <Item key={id} image={image} name={name} price={price} />
+          {fillings.map(({ image, name, price, _id }) => (
+            <Item key={_id} image={image} name={name} price={price} />
           ))}
         </WrapperGroup>
       </div>
     </section>
   );
 }
-export default BurgerInredients;
+export default BurgerIngredients;
+
 const WrapperGroup = React.forwardRef(({ title, children }, ref) => (
   <div ref={ref}>
     <div className="text text_type_main-medium">{title}</div>
@@ -176,7 +88,7 @@ const WrapperGroup = React.forwardRef(({ title, children }, ref) => (
 function Item({ image, name, price }) {
   return (
     <div className={`text text_type_main-default mb-2 ${styles.item}`}>
-      <Counter count={1} size="default" />
+      <Counter count={0} size="default" />
       <img className="ml-4 mr-4" src={image} alt={name} />
       <div className={`mt-1 mb-1 ${styles.price}`}>
         <span className="text text_type_digits-default mr-2">{price}</span>
