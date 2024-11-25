b5b44895b6270ee62a22db256930276c77ef14b3
Evgen <kapranov.evgeniy@gmail.com>
2024-11-25 17:04:31 +0300

    fix register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -3,61 +3,54 @@ import {
   PasswordInput,
   Input,
   EmailInput,
-} from '@ya.praktikum/react-developer-burger-ui-components';
-import { Link } from 'react-router-dom';
-import style from './login.module.css';
-import { useRegisterMutation } from '../services/api/auth';
-import { useState } from 'react';
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { Link, useNavigate } from "react-router-dom";
+import style from "./login.module.css";
+import { useRegisterMutation } from "../services/api/auth";
+import { useForm } from "../components/hooks/useForm";
 
 export function Register() {
-  const [value, setValue] = useState({
-    name: '',
-    email: '',
-    password: '',
+  const navigate = useNavigate();
+
+  const { values, handleChange } = useForm({
+    name: "",
+    email: "",
+    password: "",
   });
 
-  const onFormChange = (e) => {
-    setValue({
-      ...value,
-      [e.target.name]: e.target.value,
-    });
+  const [register, { isSuccess }] = useRegisterMutation();
+
+  const onRegister = async (e) => {
+    e.preventDefault();
+    await register(values);
   };
-
-  const [register] = useRegisterMutation();
-
-  const onRegister = () => {
-    register(value);
-  };
-
+  if (isSuccess) {
+    navigate("/");
+  }
   return (
     <section className={`${style.wr} ${style.textCenter}`}>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={onRegister}>
         <p className="text text_type_main-medium mb-6 mt-20">Регистрация</p>
         <Input
-          onChange={onFormChange}
-          value={value.name}
-          name={'name'}
-          placeholder={'Имя'}
+          onChange={handleChange}
+          value={values.name}
+          name={"name"}
+          placeholder={"Имя"}
           extraClass="mb-6"
         />
         <EmailInput
-          onChange={onFormChange}
-          value={value.email}
-          name={'email'}
+          onChange={handleChange}
+          value={values.email}
+          name={"email"}
           extraClass="mb-6"
         />
         <PasswordInput
-          onChange={onFormChange}
-          value={value.password}
-          name={'password'}
+          onChange={handleChange}
+          value={values.password}
+          name={"password"}
           extraClass="mb-6"
         />
-        <Button
-          type="primary"
-          size="large"
-          htmlType="submit"
-          onClick={onRegister}
-        >
+        <Button type="primary" size="large" htmlType="submit">
           <p className="text text_type_main-default">Зарегистрироваться</p>
         </Button>
       </form>
