f1308ae9c2563734f5abe51a4dd3f28129d5928e
Evgen <kapranov.evgeniy@gmail.com>
2024-11-25 16:25:58 +0300

    на проверку

--- src/services/orderDetailsSlice.js
+++ src/services/orderDetailsSlice.js
@@ -8,16 +8,12 @@ const initialState = {
   orderRequestError: false,
 };
 
-// Создаем thunk, который будет отправлять пост запрос на сервер
-// с данными о заказе (ingredients)
 export const postOrder = createAsyncThunk(
   'order/postOrder',
   async (orderDetails, {dispatch}) => {
     const body = {
       ingredients: orderDetails,
     };
-    // после отправки запроса очищаем ингредиенты в конструкторе
-    
     return await postData(`${BASE_URL}/orders`, body).then(
       (response) => {
         dispatch(resetIngredients());
@@ -32,27 +28,22 @@ export const orderDetailsSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    // pending - ожидание ответа от сервера
+    
     builder.addCase(postOrder.pending, (state) => {
       state.orderRequest = true;
       state.orderRequestError = false;
     }),
-      // fulfilled - ответ от сервера получен
+      
       builder.addCase(postOrder.fulfilled, (state, action) => {
         console.log('API Response:', action.payload);
-        // если ответ от сервера содержит номер заказа
+       
         if (action.payload && action.payload.order) {
-          // то кладем его в стейт
           state.order = action.payload.order.number;
-          
         } else {
-          // если номер заказа не пришел, то кладем ошибку
           state.orderRequestError = true;
         }
-        // и убираем флаг ожидания
         state.orderRequest = false;
       });
-    // rejected - ошибка при отправке запроса
     builder.addCase(postOrder.rejected, (state) => {
       state.orderRequest = false;
       state.orderRequestError = true;
